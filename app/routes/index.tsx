edddc119e0c096e6c2ad5f2bfcf6c523b1cea6c3
Graham McGregor <graham@gmcgregor.ca>
2021-12-09 23:54:39 -0500

    basic homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,8 +3,8 @@ import type { MetaFunction } from "remix";
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!",
+    title: "Graham McGregor",
+    description: "Graham McGregor's website and blog",
   };
 };
 
@@ -13,7 +13,19 @@ export default function Index() {
   return (
     <div className="remix__page">
       <main>
-        <h2>Welcome to Remix!</h2>
+        <h1>Graham McGregor</h1>
+        <h2>About Me</h2>
+        {/* TODO insert picture */}
+        <p>
+          Graham McGregor is an enthusiastic Software Developer, currently
+          working at Trend Micro building the next generation of continuos
+          delivery for Cloud One. He cares deeply about building high quality
+          tools to improve developer experience.
+        </p>
+        <p>
+          Graham lives in Ontario, Canada. He enjoys cooking, indoor rock
+          climbing, board games, Dungeons &amp; Dragons, and playing piano.
+        </p>
       </main>
     </div>
   );
