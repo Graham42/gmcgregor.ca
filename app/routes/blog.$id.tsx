2c0b145bebc3cab20c1dba62a623167cb39ebd27
Graham McGregor <graham@gmcgregor.ca>
2021-12-14 21:40:35 -0500

    Redirect at the routing level
    This will be faster that actually invoking the serverless function just
    to get a redirect.
    
    See https://vercel.com/docs/cli#project-configuration/redirects

--- app/routes/blog.$id.tsx
+++
@@ -1,6 +0,0 @@
-// this file needs to exist so legacy /blog/X posts can redirect
-import { LoaderFunction, redirect } from "remix";
-export const loader: LoaderFunction = ({ request }) => {
-  let target = request.url.replace(/\/blog\/?/, "/writing/");
-  return redirect(target, 301);
-};
--- app/routes/blog.tsx
+++
@@ -1,5 +0,0 @@
-import { LoaderFunction, redirect } from "remix";
-export const loader: LoaderFunction = ({ request }) => {
-  let target = request.url.replace(/\/blog\/?/, "/writing/");
-  return redirect(target, 301);
-};
